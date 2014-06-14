b7bd5528efd88997b9a877528fe1fe7585fe7248
KanmaniSelvan <kanmaniselvan@gmail.com>
2014-06-14 12:38:21 +0530

    finally logged in using simple db select model

--- app/controllers/fake_controller.rb
+++ app/controllers/fake_controller.rb
@@ -1,18 +1,39 @@
 class FakeController < ApplicationController
   def index
   end
-
-  def create
+def create
 @username=params[:username]
 @password=params[:password]
 @email=params[:email]
-
 @fakeobject = Fake.new(:username => @username, :password => @password, :email =>@email)
 @fakeobject.save
 redirect_to :controller => "fake", :action => 'show'
-  end
+end
 
-  def show
+def show
+@user=params[:user]
+@pass=params[:pass]
+@fakelogin=Fake.find_by(:username =>  @user, :password => @pass)
+if @fakelogin.nil?
+redirect_to :controller => "fake", :action => 'index'
+else
+
 @fakeshow = Fake.all
-  end
+
+end
+end
+=begin
+def check
+@user=params[:user]
+@pass=params[:pass]
+@fakelogin=Fake.find_by(:username =>  @user, :password => @pass)
+if @fakelogin.nil?
+redirect_to :controller => "fake", :action => 'index'
+else
+redirect_to :controller => "fake", :action => 'show'
+end
+end
+=end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
 
   get 'fake/create'
 
-  get 'fake/show'
+  get 'fake/show' 
+  get 'fake/check'
 
+post 'fake/index'
+
+  post 'fake/create'
+
+  post 'fake/show'
+  post 'fake/check'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
